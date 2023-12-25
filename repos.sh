fb75d5e926cbe92e2d62ba92a99b175ab30710f2
Dhouha11 <115534065+Dhouha11@users.noreply.github.com>
2023-12-25 16:21:57 +0100

    Update repos.sh

---
+++ repos.sh
@@ -0,0 +1,13 @@
+# Install VirtualBox, Ansible and Python3-pip
+sudo apt install virtualbox ansible python3-pip
+
+#Install gnup software-properties-common and curl 
+sudo apt-get install -y gnupg software-properties-common curl
+
+# Vagrant repo adding and install
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+# Install Vagrant version=2.3.4 on Ubuntu 22.04.3 LTS to prevent the compatibility problem with ansible
+sudo apt update && sudo apt install vagrant=2.3.4
+sudo apt-mark hold vagrant=2.3.4
